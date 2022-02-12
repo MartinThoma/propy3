c70c6edfac77610dc6dc1bb434fb947b424faa70
Martin Thoma <info@martin-thoma.de>
2022-02-12 09:36:18 +0100

    MAINT: Add deprecation warning for Python <=3.7

--- propy/__init__.py
+++ propy/__init__.py
@@ -1,5 +1,17 @@
 # Core Library
 from typing import List
+import sys
+import warnings
+
+_python_version = sys.version_info
+
+if _python_version.major == 2:
+    warnings.warn("Python 2 is not supported. Please use Python 3.")
+if _python_version.major == 3 and _python_version.minor < 8:
+    warnings.warn(
+        "Python 3.6 and Python 3.7 might get deprecated. "
+        "Please participate in the discussion: https://github.com/MartinThoma/propy3/issues/12"
+    )
 
 AALetter: List[str] = list("ARNDCEQGHILKMFPSTWYV")
 
